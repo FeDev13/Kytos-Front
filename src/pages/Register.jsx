f708ade6a5d256fa757a215a9ea278818df61c0e
Federico Robledo <federicorobledo85@gmail.com>
2023-11-16 12:50:36 -0300

    first commit

---
+++ src/pages/Register.jsx
@@ -0,0 +1,153 @@
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { MyContext } from "../context/PatientContext";
+import { Loader } from "../components/Loader";
+import logo from "../assets/Logo kytos_page-0001.jpg";
+
+export const Register = () => {
+  const navigate = useNavigate();
+  const [registerValues, setRegisterValues] = useState({
+    name: "",
+    lastName: "",
+    license: "",
+    password: "",
+  });
+
+  const {
+    errorMsgBack,
+    setErrorMsgBack,
+    errorMsgValidator,
+    setErrorMsgValidator,
+    loadingAuth,
+    setLoadingAuth,
+  } = useContext(MyContext);
+
+  useEffect(() => {
+    setErrorMsgBack("");
+    setErrorMsgValidator("");
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setRegisterValues({ ...registerValues, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoadingAuth(true);
+    setErrorMsgBack("");
+    setErrorMsgValidator("");
+    try {
+      await axios.post("http://localhost:5055/auth/register", registerValues);
+      setLoadingAuth(false);
+      navigate("/");
+    } catch (error) {
+      setLoadingAuth(false);
+      if (error.response.data.msg) {
+        console.log(error.response.data.msg);
+        setErrorMsgBack(error.response.data.msg);
+      }
+      if (error.response.data.errors[0].msg) {
+        console.log(error.response.data.errors[0].msg);
+        setErrorMsgValidator(error.response.data.errors[0].msg);
+      }
+      //console.log(error.response.data.msg);
+    }
+  };
+
+  return (
+    <div className="w-full h-screen m-auto bg-logo flex justify-center items-center p-10">
+      <div className="flex-col md:flex-row w-full gap-10  flex justify-start items-center">
+        <div className="w-1/2 flex justify-center items-center">
+          <img
+            src={logo}
+            alt="Register Image"
+            className="w-full rounded-3xl "
+          />
+        </div>
+        <div className="w-1/2 h-full  font-PTSans">
+          <h1 className="text-4xl text-center font-bold text-secondary">
+            Registro
+          </h1>
+          <p className="text-lg text-center font-bold text-secondary mt-2">
+            Sr. profesional, complete el formulario con sus datos para
+            registrarse a la plataforma
+          </p>
+
+          <form
+            className="w-2/3 m-auto flex flex-col justify-center items-center gap-5 mt-8"
+            onSubmit={handleSubmit}
+          >
+            <input
+              className="w-full rounded-lg h-11 bg-transparent border border-solid-white outline-none pl-3 font-PTSans text-secondary"
+              type="text"
+              name="name"
+              value={registerValues.name}
+              placeholder="Nombre"
+              onChange={handleChange}
+            />
+            <input
+              className="w-full rounded-lg h-11 bg-transparent border border-solid-white outline-none pl-3 font-PTSans text-secondary"
+              type="text"
+              name="lastName"
+              value={registerValues.lastName}
+              placeholder="Last name"
+              onChange={handleChange}
+            />
+            <input
+              className="w-full rounded-lg h-11 bg-transparent border border-solid-white outline-none pl-3 font-PTSans text-secondary"
+              type="password"
+              name="password"
+              value={registerValues.password}
+              placeholder="Password"
+              onChange={handleChange}
+            />
+            <input
+              className="w-full rounded-lg h-11 bg-transparent border border-solid-white outline-none pl-3 font-PTSans text-secondary"
+              type="text"
+              name="license"
+              value={registerValues.license}
+              placeholder="Matricula/DNI"
+              onChange={handleChange}
+            />
+            {loadingAuth ? (
+              <Loader />
+            ) : (
+              <>
+                {errorMsgValidator && (
+                  <p className="font-PTSans font-bold w-full bg-red-300 text-red-600 text-center">
+                    {errorMsgValidator}
+                  </p>
+                )}
+                {errorMsgBack && (
+                  <p className="font-PTSans font-bold w-full bg-red-300 text-red-600 text-center">
+                    {errorMsgBack}
+                  </p>
+                )}
+              </>
+            )}
+            <button
+              className="w-full rounded-lg h-11 bg-transparent border-2 border-secondary text-secondary shadow-lg hover:bg-secondary hover:border-white hover:text-primary hover:transition-all disabled:opacity-30 disabled:cursor-not-allowed"
+              type="submit"
+              disabled={
+                registerValues.name === "" ||
+                registerValues.lastName === "" ||
+                registerValues.license === "" ||
+                registerValues.password === ""
+              }
+            >
+              Register
+            </button>
+          </form>
+          <p className="text-white text-center font-bold mt-2 text-sm">
+            Si ya esta registrado ingrese{" "}
+            <small className="font-PTSans text-white text-lg underline">
+              <Link to={"/"}>aqui</Link>
+            </small>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/pages/RegisterNewPatients.jsx
@@ -0,0 +1,344 @@
+import { useContext, useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Sidebar } from "../components/Sidebar";
+import { Topbar } from "../components/Topbar";
+import { MyContext } from "../context/PatientContext";
+import { Loader } from "../components/Loader";
+import { useCookies } from "react-cookie";
+
+export const RegisterNewPatients = () => {
+  const professionalID = window.localStorage.getItem("professionalID");
+  const [cookies] = useCookies(["access_token"]);
+  const { loadingRegisterPatient, setLoadingRegisterPatient } =
+    useContext(MyContext);
+
+  const params = useParams();
+  const patientID = params.id;
+  const getPatientById = async () => {
+    try {
+      const res = await axios.get(
+        `http://localhost:5055/patients/${professionalID}/${patientID}`
+      );
+
+      setNewPatient(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (patientID) {
+      getPatientById();
+    }
+  }, []);
+
+  const [newPatient, setNewPatient] = useState({
+    name: "",
+    lastName: "",
+    age: 0,
+    weight: 0,
+    height: 0,
+    diagnostic: "",
+    symptoms: [],
+    image: "",
+    professionalID: professionalID,
+  });
+
+  const createNewPatient = async () => {
+    setLoadingRegisterPatient(true);
+    const form = new FormData();
+
+    for (let key in newPatient) {
+      form.append(key, newPatient[key]);
+    }
+
+    try {
+      const res = await axios.post(
+        "http://localhost:5055/patients/registerPatient",
+        form,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            authorization: cookies.access_token,
+          },
+        }
+      );
+      setLoadingRegisterPatient(false);
+      //console.log(res.data);
+      toast.success(res.data.msg, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+      setNewPatient({
+        name: "",
+        lastName: "",
+        age: 0,
+        weight: 0,
+        height: 0,
+        diagnostic: "",
+        symptoms: [],
+        image: "",
+        professionalID: professionalID,
+      });
+    } catch (error) {
+      setLoadingRegisterPatient(false);
+      toast.error(error);
+    }
+  };
+
+  const updatePatient = async () => {
+    setLoadingRegisterPatient(true);
+
+    try {
+      const res = await axios.put(
+        `http://localhost:5055/patients/update/${professionalID}/${patientID}`,
+        newPatient
+      );
+      setLoadingRegisterPatient(false);
+      toast.success("Paciente registrado exitosamente", {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewPatient({
+      ...newPatient,
+      [name]: value,
+    });
+  };
+
+  const handleSymptomChange = (e, index) => {
+    const { value } = e.target;
+    // console.log({value});
+    // console.log({index});
+    const symptomsCopy = newPatient.symptoms;
+    symptomsCopy[index] = value;
+    setNewPatient({
+      ...newPatient,
+      symptoms: symptomsCopy,
+    });
+  };
+
+  const addSymptoms = () => {
+    setNewPatient({
+      ...newPatient,
+      symptoms: [...newPatient.symptoms, ""],
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    //console.log(newPatient);
+    e.preventDefault();
+    if (patientID) {
+      updatePatient();
+    } else {
+      createNewPatient();
+    }
+  };
+
+  const isDisabled = () => {
+    if (
+      newPatient.name === "" ||
+      newPatient.lastName === "" ||
+      newPatient.age === 0 ||
+      newPatient.weight === 0 ||
+      newPatient.height === 0 ||
+      loadingRegisterPatient === true
+    )
+      return true;
+  };
+
+  return (
+    <div>
+      <div>
+        <Topbar />
+      </div>
+      <div className="w-full h-screen m-auto bg-[#F1F5F9] md:flex">
+        <Sidebar />
+        {/* CONTAINER FORM REGISTER PATIENT PAGE */}
+        <div className="w-full h-screen font-PTSans bg-logo absolute -mt-96 md:relative md:mt-0">
+          {/* FORM SECTION */}
+          <div className="p-4 h-[calc(100%-64px) w-[70%] mx-auto rounded-md bg-white">
+            <h1 className="text-center w-3/4 m-auto px-4 font-PTSans font-bold text-3xl text-logo bg-white">
+              Ingrese paciente nuevo{" "}
+            </h1>
+            <div className="m-auto p-4 flex justify-center items-center mt-8 bg-white ">
+              <form
+                className=" flex flex-col w-80 max-w-xl bg-white"
+                onSubmit={handleSubmit}
+              >
+                <label
+                  className="font-PTSans font-bold text-logo text-lg bg-white "
+                  htmlFor="name"
+                >
+                  Nombre:
+                </label>
+                <input
+                  className="rounded-sm h-7 bg-transparent border border-solid border-tertiary outline-none px-3 py-1 font-PTSans text-terborder-tertiary "
+                  type="text"
+                  name="name"
+                  value={newPatient.name}
+                  id="name"
+                  onChange={handleChange}
+                  autoComplete="off"
+                />
+                <label
+                  className="font-PTSans font-bold text-logo text-lg mt-2 bg-white"
+                  htmlFor="lastName"
+                >
+                  Apellido:
+                </label>
+                <input
+                  className="rounded-sm h-7 bg-transparent border border-solid border-tertiary outline-none px-3 py-1 font-PTSans text-terborder-tertiary "
+                  type="text"
+                  name="lastName"
+                  value={newPatient.lastName}
+                  id="lastName"
+                  onChange={handleChange}
+                  autoComplete="off"
+                />
+                <label
+                  className="font-PTSans font-bold text-logo text-lg mt-2 bg-white"
+                  htmlFor="age"
+                >
+                  Edad:
+                </label>
+                <input
+                  className="rounded-sm h-7 bg-transparent border border-solid border-tertiary outline-none px-3 py-1 font-PTSans text-terborder-tertiary "
+                  type="number"
+                  name="age"
+                  value={newPatient.age || ""}
+                  id="age"
+                  onChange={handleChange}
+                  autoComplete="off"
+                />
+                <label
+                  className="font-PTSans font-bold text-logo text-lg mt-2 bg-white"
+                  htmlFor="weight"
+                >
+                  Peso (kg):
+                </label>
+                <input
+                  className="rounded-sm h-7 bg-transparent border border-solid border-tertiary outline-none px-3 py-1 font-PTSans text-terborder-tertiary "
+                  type="number"
+                  name="weight"
+                  value={newPatient.weight || ""}
+                  id="weight"
+                  onChange={handleChange}
+                  autoComplete="off"
+                />
+                <label
+                  className="font-PTSans font-bold text-logo text-lg mt-2 bg-white"
+                  htmlFor="height"
+                >
+                  Altura (cms):
+                </label>
+                <input
+                  className="rounded-sm h-7 bg-transparent border border-solid border-tertiary outline-none px-3 py-1 font-PTSans text-terborder-tertiary "
+                  type="number"
+                  name="height"
+                  value={newPatient.height || ""}
+                  id="height"
+                  onChange={handleChange}
+                  autoComplete="off"
+                />
+                <label
+                  className="font-PTSans font-bold text-logo text-lg mt-2 bg-white"
+                  htmlFor="symptoms"
+                >
+                  Sintomas:
+                </label>
+                <div className="w-full grid grid-cols-2 justify-center gap-1">
+                  {newPatient.symptoms.map((el, index) => (
+                    <input
+                      className="w-full rounded-sm h-7 bg-transparent border border-solid border-tertiary outline-none px-3 py-1 font-PTSans text-terborder-tertiary  text-center"
+                      key={index}
+                      type="text"
+                      name="symptoms"
+                      value={el}
+                      id="symptoms"
+                      onChange={(e) => handleSymptomChange(e, index)}
+                      autoComplete="off"
+                      autoFocus={true}
+                    />
+                  ))}
+                </div>
+                <button
+                  className="w-fit px-6 font-semibold rounded-lg h-7 bg-transparent border border-green-600 text-black shadow-lg hover:bg-green-600 hover:text-white hover:transition-all disabled:opacity-20 disabled:cursor-not-allowed"
+                  type="button"
+                  onClick={addSymptoms}
+                  disabled={newPatient.symptoms.includes("")}
+                >
+                  Agregar sintomas
+                </button>
+                <label
+                  className="font-PTSans font-bold text-logo text-lg mt-2 bg-white"
+                  htmlFor="diagnostic"
+                >
+                  Diagnostico:
+                </label>
+                <textarea
+                  className="resize-none rounded-xl text-sm  bg-transparent border border-solid border-tertiary outline-none px-3 py-1 font-PTSans text-terborder-tertiary"
+                  name="diagnostic"
+                  value={newPatient.diagnostic}
+                  id="diagnostic"
+                  onChange={handleChange}
+                  autoComplete="off"
+                  cols={50}
+                  rows={2}
+                />
+                <label
+                  className="font-PTSans font-bold text-logo text-lg mt-2 bg-white"
+                  htmlFor="image"
+                >
+                  Foto de perfil:
+                </label>
+                <input
+                  className="block w-full text-sm text-logo border border-logo rounded-sm cursor-pointer bg-secondary dark:text-secondary focus:outline-none dark:bg-logo dark:border-logo dark:placeholder-secondary"
+                  type="file"
+                  name="image"
+                  id="image"
+                  onChange={(e) =>
+                    setNewPatient({ ...newPatient, image: e.target.files[0] })
+                  }
+                />
+                {loadingRegisterPatient && (
+                  <div className="w-full m-auto flex justify-center items-center mt-2">
+                    <Loader />
+                  </div>
+                )}
+                <div className="my-4  text-center bg-white">
+                  <button
+                    className="w-[50%] mx-auto font-semibold rounded-lg p-1 bg-transparent border-2 border-logo text-logo  shadow-lg hover:bg-logo hover:text-white hover:transition-all disabled:opacity-50 disabled:cursor-not-allowed"
+                    type="submit"
+                    disabled={isDisabled()}
+                  >
+                    {patientID
+                      ? "Actualizar paciente"
+                      : "Registrar nuevo paciente"}
+                  </button>
+                </div>
+
+                <Link
+                  className="w-[50%] mx-auto font-semibold rounded-lg p-1  border border-red-400 text-black bg-secondary shadow-lg hover:bg-red-600 hover:text-white hover:transition-all text-center"
+                  to="/home"
+                >
+                  Cerrar
+                </Link>
+              </form>
+              <ToastContainer autoClose={2500} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
